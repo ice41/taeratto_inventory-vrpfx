d69f3fe8eb8e5c157a1c7d2bdd421ecb79a50c5f
Eduardo <edu.ice7@gmail.com>
2020-03-04 00:19:12 +0000

    Create pt.lua

---
+++ locales/pt.lua
@@ -0,0 +1,11 @@
+Locales["pt"] = {
+	["cash"] = "Dinheiro",
+	["black_money"] = "Dinheiro Sujo",
+	["player_nearby"] = "Jogador Proximo!",
+	["players_nearby"] = "Jogadores Proximos!",
+	["openinv_help"] = "Abrir inventario de outros jogador",
+	["openinv_id"] = "ID do jogador",
+	["no_permissions"] = "sen permissão!",
+	["no_player"] = "Não foi possível encontrar o player com o ID especificado.!",
+	["player_inventory"] = "Inventario do Jogador"
+}
